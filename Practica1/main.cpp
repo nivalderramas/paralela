67f0cfbf9172dce78afae4aa13372a237cf10595
juromerop <juromerop@unal.edu.co>
2023-10-05 20:40:38 -0500

    creating a new video with the vector of frames

--- Practica1/main.cpp
+++ Practica1/main.cpp
@@ -1,43 +1,47 @@
-//
-//  main.cpp
-//  VideoDowngrader
-//
-//  Created by Juan Pablo Garzon Parra on 5/10/23.
-//
-
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
     cv::VideoCapture cap("video.mp4");
-    
+
     if (!cap.isOpened()) {
-        cerr << "Error al abrir el video." << endl;
+        cerr << "Error" << endl;
         return -1;
     }
 
-
-    cv::Mat frame;
     int frameWidth = cap.get(cv::CAP_PROP_FRAME_WIDTH);
     int frameHeight = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
     int fps = cap.get(cv::CAP_PROP_FPS);
-    int frameCount = cap.get(cv::CAP_PROP_FRAME_COUNT);
 
-    cout << "El video tiene " << frameCount << " frames en total." << endl;
-    cout << "El video tiene " << frameHeight << " pixeles de alto." << endl;
+    cv::VideoWriter outputVideo("nuevo_video.mp4", cv::VideoWriter::fourcc('H', '2', '6', '4'), fps, cv::Size(frameWidth, frameHeight), true);
+
+
+    if (!outputVideo.isOpened()) {
+        cerr << "Error" << endl;
+        return -1;
+    }
 
     vector<cv::Mat> frames;
+    cv::Mat frame;
+
     while (cap.read(frame)) {
         frames.push_back(frame);
     }
 
-    cout << "Se han leido " << frames.size() << " frames." << endl;
+    // cout << "Se han leído " << frames.size() << " fotogramas." << endl;
 
+    for (const cv::Mat& frame : frames) {
+        outputVideo.write(frame);
+    }
 
+    cout << "Se ha creado el nuevo video 'nuevo_video.mp4'." << endl;
 
     cap.release();
+    outputVideo.release();
     cv::destroyAllWindows();
+
     return 0;
 }
